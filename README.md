ec518ed54037bc558b5f07217692cc766074917f
DavidDDG94 <daviddroguettgallardo@gmail.com>
2025-01-26 23:17:01 -0300

    funcionalidades-agregar-y-listar-ok

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 Primer challenge correspondiente al desafio Formacion principiante en programacion de Oracle Next Educatiom-Alura Latam.
 Consiste en desarrollar una aplicacion que perite a los usuarios ingrsar nombres y realizar el sorteo aleatorio de uno de los nombres ingresados.
+
+Funcionalidades
+Se activa una alerta en pantalla si el campo esta vacio.
+si el usuario ya se encuntra en la lista igualmente indica que el isuario ya ecxiste y no lo inserta.
--- app.js
+++ app.js
@@ -1,1 +1,43 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+//Funcion para limpiar input
+function limpiarinput(){
+    caja = document.querySelector('#amigo').value = '';
+}
+
+//Funcion para ahgregar amigos al array
+function agregarAmigo(){
+    let amigo = document.getElementById('amigo').value;
+
+    if(amigo === ''){
+        alert('Por favor, inserte un nombre.');
+        return;
+    } else{
+            if(amigos.includes(amigo)){
+                alert('El amigo ya existe en la lista.');
+            } else{
+                amigos.push(amigo);
+                console.log(amigos);
+                console.log(typeof (amigo));
+            }
+        //Limpiar input
+        limpiarinput();    
+    }
+    listarAmigos();
+}
+
+//Funcion para mostrar lista de amigos agregados
+function listarAmigos(){
+    let lista = document.getElementById('listaAmigos');   
+    lista.innerHTML = '';
+    for (let i = 0; i < amigos.length; i++) {
+        // Crear un nuevo elemento <li>
+        const li = document.createElement("li");
+        // Asignar el nombre del amigo al contenido del <li>
+        li.textContent = amigos[i];
+        // Agregar el <li> a la lista
+        lista.appendChild(li);
+    }
+}
